72335074e0f7888ab95f7d91f72804fe73153da7
Antti Lukats <Antti.Lukats@googlemail.com>
2019-08-15 18:48:44 +0200

    add mega65

--- litex_boards/partner/targets/mega65.py
+++ litex_boards/partner/targets/mega65.py
@@ -8,7 +8,7 @@ import argparse
 
 from migen import *
 
-from litex_boards.partner.platforms import s7mini
+from litex_boards.partner.platforms import mega65
 
 from litex.soc.interconnect import wishbone
 
@@ -27,7 +27,8 @@ class _CRG(Module):
         # # #
         self.cd_sys.clk.attr.add("keep")
         self.submodules.pll = pll = S7PLL(speedgrade=-1)
-        self.comb += pll.reset.eq(~platform.request("cpu_reset"))
+        self.comb += pll.reset.eq(platform.request("cpu_reset"))
+
         pll.register_clkin(platform.request("clk100"), 100e6)
         pll.create_clkout(self.cd_sys, sys_clk_freq)
 
@@ -36,21 +37,33 @@ class _CRG(Module):
 
 class BaseSoC(SoCCore):
     mem_map = {
-        "hyperram": 0x40000000,
+#        "spiflash": 0x20000000,
+        "hyperram": 0x20000000,
     }
     mem_map.update(SoCCore.mem_map)
 
     def __init__(self, sys_clk_freq=int(100e6), **kwargs):
-        platform = s7mini.Platform()
+        platform = mega65.Platform()
 
         SoCCore.__init__(self, platform, clk_freq=sys_clk_freq,
+            ident="MEGA65", ident_version=True,
             integrated_rom_size=0x8000,
-#            integrated_main_ram_size=0x8000,
+            integrated_main_ram_size=0x10000,
             **kwargs)
 
 	# can we just use the clock without PLL ?
 
         self.submodules.crg = _CRG(platform, sys_clk_freq)
+        self.counter = counter = Signal(32)
+        self.sync += counter.eq(counter + 1)
+ 
+	#
+        led_red = platform.request("user_led", 0)
+        self.comb += led_red.eq(counter[23])
+
+#        led_green = platform.request("user_led_green")
+#        self.comb += led_green.eq(counter[25])
+
 
         hyperram_pads = platform.request("hyperram")
         self.submodules.hyperram = HyperRAM(
@@ -61,22 +74,10 @@ class BaseSoC(SoCCore):
             "hyperram", self.mem_map["hyperram"] | self.shadow_base, 8*1024*1024)
 
 
-        self.counter = counter = Signal(32)
-        self.sync += counter.eq(counter + 1)
- 
-	#
-        led_red = platform.request("user_led_red")
-        self.comb += led_red.eq(counter[23])
-
-        led_green = platform.request("user_led_green")
-        self.comb += led_green.eq(counter[25])
-
-
-
 # Build --------------------------------------------------------------------------------------------
 
 def main():
-    parser = argparse.ArgumentParser(description="LiteX on Spartan-7 mini")
+    parser = argparse.ArgumentParser(description="LiteX on MEGA65")
     builder_args(parser)
 #    soc_sdram_args(parser)
     soc_core_args(parser)
